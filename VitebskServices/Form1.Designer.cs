d942c64c0f67a8fdf61237ffc95d9f7003907c6e
Elway1233 <zheny123324432@mail.ru>
2022-04-19 16:02:49 +0300

    Добавьте файлы проекта.

---
+++ VitebskServices/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace VitebskServices
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.Services = new System.Windows.Forms.ToolStripMenuItem();
+            this.Head = new System.Windows.Forms.ToolStripMenuItem();
+            this.Products = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Entertainment = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Services});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Services
+            // 
+            this.Services.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Head,
+            this.Products,
+            this.Entertainment});
+            this.Services.Name = "Services";
+            this.Services.Size = new System.Drawing.Size(57, 20);
+            this.Services.Text = "Услуги";
+            // 
+            // Head
+            // 
+            this.Head.Name = "Head";
+            this.Head.Size = new System.Drawing.Size(180, 22);
+            this.Head.Text = "Парикмахерские";
+            this.Head.Click += new System.EventHandler(this.Head_Click);
+            // 
+            // Products
+            // 
+            this.Products.Name = "Products";
+            this.Products.Size = new System.Drawing.Size(180, 22);
+            this.Products.Text = "Продукты";
+            this.Products.Click += new System.EventHandler(this.Products_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 44);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(602, 405);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Entertainment
+            // 
+            this.Entertainment.Name = "Entertainment";
+            this.Entertainment.Size = new System.Drawing.Size(180, 22);
+            this.Entertainment.Text = "Развлечения";
+            this.Entertainment.Click += new System.EventHandler(this.Entertainment_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "IS";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Services;
+        private System.Windows.Forms.ToolStripMenuItem Head;
+        private System.Windows.Forms.ToolStripMenuItem Products;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem Entertainment;
+    }
+}
+
---
+++ VitebskServices/Form1.cs
@@ -0,0 +1,87 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VitebskServices
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Head_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            MySqlConnection ThisConnection = new MySqlConnection("server=localhost;port=3307;username=root;password=root;database=is");
+            ThisConnection.Open();
+            MySqlCommand thisCommand = ThisConnection.CreateCommand();
+            thisCommand.CommandText = "SELECT * FROM `hairdressing` WHERE `Service` = 'Парикмахерская'";
+            MySqlDataReader thisReader = thisCommand.ExecuteReader();
+            string res = string.Empty;
+            while (thisReader.Read())
+            {
+                res += ("Название: ") + thisReader["Name"] + Environment.NewLine;
+                res += ("Адрес: ") + thisReader["Address"] + Environment.NewLine;
+                res += ("Номер телефона: ") + thisReader["Telephone"] + Environment.NewLine;
+                res += ("График работ: ") + thisReader["WorkTime"] + Environment.NewLine;
+                res += ("Сайт: ") + thisReader["WebSite"] + Environment.NewLine + Environment.NewLine;
+            }
+
+            thisReader.Close();
+            ThisConnection.Close();
+            textBox1.Text += res;
+        }
+
+        private void Products_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            MySqlConnection ThisConnection = new MySqlConnection("server=localhost;port=3307;username=root;password=root;database=is");
+            ThisConnection.Open();
+            MySqlCommand thisCommand = ThisConnection.CreateCommand();
+            thisCommand.CommandText = "SELECT * FROM `Products` WHERE `Service` = 'Продукты'";
+            MySqlDataReader thisReader = thisCommand.ExecuteReader();
+            string res = string.Empty;
+            while (thisReader.Read())
+            {
+                res += ("Название: ") + thisReader["Name"] + Environment.NewLine;
+                res += ("Адрес: ") + thisReader["Address"] + Environment.NewLine;
+                res += ("Номер телефона: ") + thisReader["Telephone"] + Environment.NewLine;
+                res += ("График работ: ") + thisReader["WorkTime"] + Environment.NewLine;
+                res += ("Сайт: ") + thisReader["WebSite"] + Environment.NewLine + Environment.NewLine;
+            }
+            thisReader.Close();
+            ThisConnection.Close();
+            textBox1.Text += res;
+        }
+
+        private void Entertainment_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            MySqlConnection ThisConnection = new MySqlConnection("server=localhost;port=3307;username=root;password=root;database=is");
+            ThisConnection.Open();
+            MySqlCommand thisCommand = ThisConnection.CreateCommand();
+            thisCommand.CommandText = "SELECT * FROM `Entertainment` WHERE `Service` = 'Развлечения'";
+            MySqlDataReader thisReader = thisCommand.ExecuteReader();
+            string res = string.Empty;
+            while (thisReader.Read())
+            {
+                res += ("Название: ") + thisReader["Name"] + Environment.NewLine;
+                res += ("Адрес: ") + thisReader["Address"] + Environment.NewLine;
+                res += ("Номер телефона: ") + thisReader["Telephone"] + Environment.NewLine;
+                res += ("График работ: ") + thisReader["WorkTime"] + Environment.NewLine;
+                res += ("Сайт: ") + thisReader["WebSite"] + Environment.NewLine + Environment.NewLine;
+            }
+            thisReader.Close();
+            ThisConnection.Close();
+            textBox1.Text += res;
+        }
+    }
+}
